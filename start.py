ac86dff8077f9542939ad3a6b395a317de7a0183
icentasy <icentasy@gmail.com>
2016-03-28 10:05:57 +0800

    [change]change start

--- start.py
+++ start.py
@@ -2,7 +2,8 @@
 import sys
 sys.path.append("./")
 from queue import RedisQueue
+from mysql import MySQL
 
 requestQ = RedisQueue("request", host="115.159.69.54")
-
-
+con = MySQL(host="115.159.69.54", db="bank", user="root", passwd="gyhy")
+print con.login('1','1')
