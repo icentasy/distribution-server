1784e14ebfea6107f8b10b36dfb29b50cb51a888
icentasy <icentasy@gmail.com>
2016-03-28 22:35:15 +0800

    change

--- start.py
+++ start.py
@@ -10,19 +10,12 @@ requestQ = RedisQueue("request", host="115.159.69.54")
 con = MySQL("115.159.69.54", 3306, user="root", passwd="gyhy")
 
 while  True:
-	if requestQ.get() as req is None:
+	req = requestQ.get()
+	if req is None:
 		continue
 	print "get sources"
 	reqJson = json.loads(req)
 	if reqJson.get("type") == 0:
 		print con.login(reqJson.get("id"), reqJson.get("pw"))
-		res = {
-			"status":0, 
-			"errorMsg": null, 
-			"data":{
-				"msg":"login success",
-				"sum": 
-			},
-			"checkSum":""
-		}
+		res = {"status":0}
 		requestQ.set("%s_%s" % (reqJson.get("id"), reqJson.get("timestamp")), json.dumps(res))
