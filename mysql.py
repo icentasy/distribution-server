ae3b220c11489ae9732eb1246beb9feebe188aaa
icentasy <icentasy@gmail.com>
2016-03-28 10:31:09 +0800

    change mysql

--- mysql.py
+++ mysql.py
@@ -5,7 +5,7 @@ class MySQL(object):
 		self.__conn = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
 		self.__cur = self.__conn.cursor()
 
-	def login(account, pw):
+	def login(self, account, pw):
 		self.__cur.excute("select count(*) from user where id=%s and pw=%s" % (account, pw))
 		if self.__cur.fetchOne() == 1:
 			return True
