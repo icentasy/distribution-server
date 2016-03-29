04f7bab0e7e92fedd3c2d22f203d87b86a822142
icentasy <icentasy@gmail.com>
2016-03-29 09:13:48 +0800

    add try/except

--- start.py
+++ start.py
@@ -14,58 +14,62 @@ while  True:
 	if req is None:
 		continue
 	print req
-	res = {"status":0, "errorMsg":"", "data": None}
-	resData = {"sum": 0}
-	reqJson = json.loads(req)
-	tradeType = reqJson.get("type")
-	account = reqJson.get("id")
-	pw = reqJson.get("pw")
-	timestamp = reqJson.get("timestamp")
-	data = reqJson.get("data")
-	if tradeType == 0:
-		res["status"] = con.login(account, pw)
-	elif tradeType == 2:
-		cash = data.get("cash")
-		status, total = con.withdraw(account, cash, timestamp)
-		res["status"] = status
-		if status:
+	try:
+		res = {"status":0, "errorMsg":"", "data": None}
+		resData = {"sum": 0}
+		reqJson = json.loads(req)
+		tradeType = reqJson.get("type")
+		account = reqJson.get("id")
+		pw = reqJson.get("pw")
+		timestamp = reqJson.get("timestamp")
+		data = reqJson.get("data")
+		if tradeType == 0:
+			res["status"] = con.login(account, pw)
+		elif tradeType == 2:
+			cash = data.get("cash")
+			status, total = con.withdraw(account, cash, timestamp)
+			res["status"] = status
+			if status:
+				resData["sum"] = total
+			else:
+				res["errorMsg"] = total
+		elif tradeType == 3:
+			cash = data.get("cash")
+			status, total = con.deposite(account, cash, timestamp)
+			res["status"] = status
+			if status:
+				resData["sum"] = total
+			else:
+				res["errorMsg"] = total
+		elif tradeType == 4:
+			cash = data.get("cash")
+			acID = data.get("acID")
+			acName = data.get("acName")
+			status, total = con.transfer(account, cash, timestamp, acID, acName)
+			res["status"] = status
+			if status:
+				resData["sum"] = total
+			else:
+				res["errorMsg"] = total
+		elif tradeType == 6:
+			status, total = con.checkTotal(account)
+			res["status"] = status
 			resData["sum"] = total
-		else:
-			res["errorMsg"] = total
-	elif tradeType == 3:
-		cash = data.get("cash")
-		status, total = con.deposite(account, cash, timestamp)
-		res["status"] = status
-		if status:
-			resData["sum"] = total
-		else:
-			res["errorMsg"] = total
-	elif tradeType == 4:
-		cash = data.get("cash")
-		acID = data.get("acID")
-		acName = data.get("acName")
-		status, total = con.transfer(account, cash, timestamp, acID, acName)
-		res["status"] = status
-		if status:
-			resData["sum"] = total
-		else:
-			res["errorMsg"] = total
-	elif tradeType == 6:
-		status, total = con.checkTotal(account)
-		res["status"] = status
-		resData["sum"] = total
-	elif tradeType == 7:
-		status, detail = con.checkDetail(account)
-		res["status"] = status
-		resData = []
-		for item in detail:
-			resData.append({
-				"type": item[1],
-				"cash": item[2],
-				"timestamp": item[4],
-				"transferid": item[3]
-				})
-	res["data"] = resData
-	requestQ.set("%s_%s" % (account, timestamp), json.dumps(res))
+		elif tradeType == 7:
+			status, detail = con.checkDetail(account)
+			res["status"] = status
+			resData = []
+			for item in detail:
+				resData.append({
+					"type": item[1],
+					"cash": item[2],
+					"timestamp": item[4],
+					"transferid": item[3]
+					})
+		res["data"] = resData
+		requestQ.set("%s_%s" % (account, timestamp), json.dumps(res))
+	except Exception as e:
+		print e
+		continue
 
 
