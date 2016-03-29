8183d7b49d44c26090d7313ce4e85e6c67d67059
icentasy <icentasy@gmail.com>
2016-03-29 21:28:48 +0800

    fix login bug

--- mysql.py
+++ mysql.py
@@ -7,7 +7,7 @@ class MySQL(object):
         self.__cur = self.__conn.cursor()
 
     def login(self, account, pw):
-        self.__cur.execute("select count(*) from user where id=%s and pw=%s" % (account, pw))
+        self.__cur.execute("select count(*) from user where id='%s' and pw='%s'" % (account, pw))
         if self.__cur.fetchone()[0] == 1:
             return True
         else:
